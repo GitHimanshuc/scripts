ba35e3301cb98fda25ec03a4bd55720d247cd2da
GitHimanshuc <unphys@gmail.com>
2022-01-16 11:38:56 -0800

    first

---
+++ plot_data_from_checkpoint.sh
@@ -0,0 +1,45 @@
+called_dir=$(pwd)
+
+cp_folder_input="$1"
+
+# Goto the checkpoint directory
+cd $cp_folder_input &&\
+
+cp_dir_name=${PWD##*/} &&\
+cp_folder=$(pwd) &&\
+
+#copy the checkpoint files to the called directory
+cp -r -v $cp_folder $called_dir &&\
+
+#copy of the checkpoint files in the called dir
+cp_called="$called_dir/$cp_dir_name" &&\
+
+#copy all input and txt files from the parent of the checkpoint folder
+cp -v ../../*.input $cp_called || : &&\
+cp -v ../../*.txt $cp_called || : &&\
+
+cp -v ../*.input $cp_called || : &&\
+cp -v ../*.txt $cp_called || : &&\
+
+cd $cp_called &&\
+if test -f "./GrDomain.input"; then
+    echo "GrDomain.input detected. It is a BBH simulation, using Gaugecheck_small_bbh"
+    # call the apply observer
+    ApplyObservers -t psi,kappa -r 11,122 -d 4,4 -domaininput "./GrDomain.input" -h5prefix 'Cp-VarsGr' /panfs/ds09/sxs/himanshu/scripts/helper_files/Gaugecheck_small_bbh.input
+
+else
+
+    echo "Domain.input detected. It is a single BH simulation, using Gaugecheck_small_bh"
+    # echo "Adding HistoryFile=<<NONE>>; to Domain.input"
+    # echo "HistoryFile=<<NONE>>;" >> ./Domain.input
+    # call the apply observer
+    ApplyObservers -t psi,kappa -r 11,122 -d 4,4 -domaininput "./Domain.input" -h5prefix 'Cp-Vars' /panfs/ds09/sxs/himanshu/scripts/helper_files/Gaugecheck_small_bh.input
+
+fi
+
+touch ./data_location.txt &&\
+echo $cp_folder >> ./data_location.txt &&\
+
+var_name="${cp_folder////_}" &&\
+cd .. &&\
+mv $cp_called $var_name
